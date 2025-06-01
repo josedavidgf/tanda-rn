5f8b99546a2b2843f8d12d80e78d1d109bd91f24
josedavidgf <josedavidgarciafernandez@gmail.com>
2025-06-01 12:28:24 +0100

    HeaderFirstLevel events

--- src/components/header/HeaderFirstLevel.tsx
+++ src/components/header/HeaderFirstLevel.tsx
@@ -4,7 +4,8 @@ import AppText from '../ui/AppText';
 import { useNavigation, useRoute } from '@react-navigation/native';
 import { spacing, colors, typography } from '@/styles';
 import NotificationDotIcon from '@/components/ui/NotificationDotIcon';
-
+import { trackEvent } from '@/app/hooks/useTrackPageView';
+import { EVENTS } from '@/utils/amplitudeEvents';
 
 export default function HeaderFirstLevel({ title }: { title: string }) {
   const navigation = useNavigation();
@@ -17,13 +18,22 @@ export default function HeaderFirstLevel({ title }: { title: string }) {
       <AppText variant='h2'>{title}</AppText>
       {isCalendarScreen && (
         <View style={styles.actions}>
-          <Pressable onPress={() => navigation.navigate('Stats')}>
+          <Pressable onPress={() => {
+            navigation.navigate('Stats');
+            trackEvent(EVENTS.STATS_BUTTON_CLICKED);
+          }}>
             <ChartBar size={24} weight="regular" color={colors.gray[800]} />
           </Pressable>
-          <Pressable onPress={() => navigation.navigate('ActivityList')}>
+          <Pressable onPress={() => {
+            navigation.navigate('ActivityList');
+            trackEvent(EVENTS.ACTIVITY_LIST_BUTTON_CLICKED);
+          }}>
             <NotificationDotIcon />
           </Pressable>
-          <Pressable onPress={() => navigation.navigate('ProfileMenu')}>
+          <Pressable onPress={() => {
+            navigation.navigate('ProfileMenu');
+            trackEvent(EVENTS.PROFILE_MENU_BUTTON_CLICKED);
+          }}>
             <User size={24} weight="regular" color={colors.gray[800]} />
           </Pressable>
         </View>
